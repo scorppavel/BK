20f0a5e52da82bacf2d8e5f7ada112f7039b3762
Pavel Frolov <pascorp@mail.ru>
2020-05-11 20:23:04 +0200

    Checkbox bk

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <checkbox-card></checkbox-card>
+  </div>
+</template>
+<script>
+import CheckboxCard from './components/ChekboxCard'
+export default {
+  components: {
+    CheckboxCard
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/ChekboxCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="bk-checkbox-card">
+    <div class="img-checkbox">
+      <img src="../assets/tomato.png">
+    </div>
+    <div class="text-checkbox">
+      помидор
+    </div>
+    <div class="confirm-checkbox">
+      <img src="../assets/circle.svg">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.bk-checkbox-card {
+  width: 232px;
+  height: 304px;
+  background-color: #fff;
+  box-shadow: 0px 10px 30px rgba(0, 0, 0, 0.05);
+  border-radius: 10px;
+}
+
+.img-checkbox {
+  display: flex;
+}
+
+.img-checkbox img {
+  width: 150px;
+  height: 150px;
+  margin: auto;
+}
+
+.text-checkbox {
+  color: #602F03;
+  text-align: center;
+  margin: 10px 0 32px 0;
+  font: 18px 900 sans-serif;
+}
+
+.confirm-checkbox {
+  display: flex;
+}
+
+.confirm-checkbox img {
+  width: 50px;
+  height: 50px;
+  margin: auto;
+}
+</style>
